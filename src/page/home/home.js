bc975e14bfcdee5dd1e8047455338e41bbb005c7
abhirawat623 <abhirawat623@gmail.com>
2023-07-26 21:55:51 +0530

    headings

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -4,8 +4,8 @@ export const Home =()=>{
 
     return(
 
-         <div class="home-container">
-           <h1 className="main-heading">Flauntup Browser Extn</h1>
+         <div class="home-container d-flex gap align-center justify-center direction-column">
+           <h1 className="main-heading">Flauntup Extension</h1>
              <div className="name-heading">
                  <span className="heading-1">Hello,What's your name?</span>
              </div>
